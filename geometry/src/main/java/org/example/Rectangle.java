8c75ebc3b5df16e95691901d2cb9b6133a4eb0bf
Ramcharan.Teja <ramcharan.teja@thoughtworks.com>
2023-11-06 13:23:06 +0530

    [Abi/Ramcharan] Add. the negative number case

--- geometry/src/main/java/org/example/Rectangle.java
+++ geometry/src/main/java/org/example/Rectangle.java
@@ -3,6 +3,9 @@ package org.example;
 public class Rectangle {
 
     public Integer computeArea(int length, int breadth) {
+        if(length < 0 || breadth < 0){
+            throw new NumberFormatException("Positive Number are allowed");
+        }
         return length*breadth;
     }
 }
--- geometry/src/test/java/RectangleTest.java
+++ geometry/src/test/java/RectangleTest.java
@@ -9,11 +9,14 @@ public class RectangleTest {
     @Test
     void shouldCalculateAreaOfARectangle(){
         Rectangle rectangle = new Rectangle();
-
         Integer area = rectangle.computeArea(3,5);
-
         assertEquals(15,area);
-
     }
-
+    @Test
+    void shouldThownExceptionOnNegativeNumber(){
+        Rectangle rectangle = new Rectangle();
+        Exception exception = assertThrows(NumberFormatException.class, () -> 
+                                                rectangle.computeArea(-3,5));
+        assertEquals("Positive Number are allowed", exception.getMessage());
+    }
 }
