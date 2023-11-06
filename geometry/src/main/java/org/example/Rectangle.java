400747ee04e468880fbe2999d2763e2ff83b6088
Ramcharan.Teja <ramcharan.teja@thoughtworks.com>
2023-11-06 15:53:10 +0530

    [Abi\Ramcharan] Add perimeter support to the rectangle class

--- geometry/src/main/java/org/example/Rectangle.java
+++ geometry/src/main/java/org/example/Rectangle.java
@@ -4,11 +4,21 @@ import java.math.BigInteger;
 
 public class Rectangle {
     public static final String POSTIVE_NUM_EXCEPTION = "Positive Number are allowed";
+    
+    private final int length;
+    private final int breadth;
 
-    public BigInteger calculateArea(int length, int breadth) {
-        if(length < 0 || breadth < 0){
+    public Rectangle(int length, int breadth) {
+        this.length = length;
+        this.breadth = breadth;
+        if(length <= 0 || breadth <= 0){
             throw new NumberFormatException(POSTIVE_NUM_EXCEPTION);
         }
+    }
+    public BigInteger calculateArea() {
         return BigInteger.valueOf(length*breadth);
+    }
+    public BigInteger calculatePerimeter() {
+        return BigInteger.valueOf(2*(length+breadth));
     }
 }
--- geometry/src/test/java/RectangleTest.java
+++ geometry/src/test/java/RectangleTest.java
@@ -1,5 +1,4 @@
 import org.example.Rectangle;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -9,26 +8,34 @@ import java.math.BigInteger;
 
 public class RectangleTest {
 
-    static Rectangle rectangle;
-    @BeforeAll
-    static void initSetUp(){
-        rectangle = new Rectangle();
-    }
-
     @Test
-    void shouldCalculateAreaOfARectangle_thenAssetResult(){
-        BigInteger area = rectangle.calculateArea(3,5);
-        assertEquals(15,area.longValue());
+    void shouldCalculateAreaOfARectangle_thenAssetArea(){
+        Rectangle rectangle = new Rectangle(2,3);
+        BigInteger area = rectangle.calculateArea();
+        assertEquals(6,area.longValue());
     }
     @Test
     void shouldCalculateAreaOfARectangle_thenThownExceptionOnNegativeNumber(){
         Exception exception = assertThrows(NumberFormatException.class, () -> 
-                                                rectangle.calculateArea(-3,5));
+                                                new Rectangle(-2,3));
         assertEquals(Rectangle.POSTIVE_NUM_EXCEPTION, exception.getMessage());
     }
     @Test
     void shouldCalculateAreaOfARectangle_thenResultExceedsTheIntegerRange(){
-        BigInteger area = rectangle.calculateArea(2147483647,2147483647);
+        Rectangle rectangle = new Rectangle(2147483647,2147483647);
+        BigInteger area = rectangle.calculateArea();
         assertEquals(2147483647*2147483647,area.longValue());
+    }
+    @Test
+    void shouldCalucaltePerimeterOfARectangle_thenAssetPerimeter(){
+        Rectangle rectangle = new Rectangle(2,3);
+        BigInteger perimeter = rectangle.calculatePerimeter();
+        assertEquals(10, perimeter.longValue());
+    }
+    @Test
+    void shouldCalucaltePerimeterOfARectangle_thenResultExceedsTheIntegerRange(){
+        Rectangle rectangle = new Rectangle(2147483647,2147483647);
+        BigInteger perimeter = rectangle.calculatePerimeter();
+        assertEquals(2*(2147483647+2147483647),perimeter.longValue());
     }
 }
