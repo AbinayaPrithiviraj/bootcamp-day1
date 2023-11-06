56f4192b86e88aa511999b283c327f83bb24c5b5
Abinaya.Ganesan <abinaya.ganesan@thoughtworks.com>
2023-11-06 12:58:55 +0530

    [Abi/Ramcharan] - Add create a test object

---
+++ geometry/src/test/java/RectangleTest.java
@@ -0,0 +1,10 @@
+import org.example.Rectangle;
+import org.junit.jupiter.api.Test;
+
+public class RectangleTest {
+
+    @Test
+    void shouldCalculateAreaOfARectangle(){
+        Rectangle rectangle = new Rectangle();
+    }
+}
