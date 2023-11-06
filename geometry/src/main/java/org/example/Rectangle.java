2bdf62d93f8c61003b5198dfa489c5ff247f196c
Abinaya.Ganesan <abinaya.ganesan@thoughtworks.com>
2023-11-06 13:06:20 +0530

    [Abi/Ramcharan] - Add Compute Area of Rectangle

--- geometry/src/main/java/org/example/Rectangle.java
+++ geometry/src/main/java/org/example/Rectangle.java
@@ -2,4 +2,7 @@ package org.example;
 
 public class Rectangle {
 
+    public Integer computeArea(int length, int breadth) {
+        return length*breadth;
+    }
 }
--- geometry/src/test/java/RectangleTest.java
+++ geometry/src/test/java/RectangleTest.java
@@ -1,10 +1,19 @@
 import org.example.Rectangle;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class RectangleTest {
 
     @Test
     void shouldCalculateAreaOfARectangle(){
         Rectangle rectangle = new Rectangle();
+
+        Integer area = rectangle.computeArea(3,5);
+
+        assertEquals(15,area);
+
     }
+
 }
